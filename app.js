dd79590ad00f5fc1c47bfcf22464037b4caae5b8
falaestudante654-debug <falaestudante654@gmail.com>
2026-06-07 19:57:36 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
+const form = document.getElementById("relatoForm");
+const mensagem = document.getElementById("mensagem");
 
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    mensagem.innerHTML = "✅ Relato enviado com sucesso!";
+
+    form.reset();
+});
