41f45dc1105290744be6f0c1b8dbeb5d4e9e6390
falaestudante654-debug <falaestudante654@gmail.com>
2026-06-07 20:25:02 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
+import { db } from "./firebase-config.js";
+import { collection, addDoc, serverTimestamp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
 const form = document.getElementById("relatoForm");
 const mensagem = document.getElementById("mensagem");
 
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
 
-    mensagem.innerHTML = "✅ Relato enviado com sucesso!";
+  await addDoc(collection(db, "relatos"), {
+    tipo: document.getElementById("tipo").value,
+    data: document.getElementById("data").value,
+    local: document.getElementById("local").value,
+    descricao: document.getElementById("descricao").value,
+    criadoEm: serverTimestamp()
+  });
 
-    form.reset();
+  mensagem.textContent = "✅ Relato enviado com sucesso!";
+  form.reset();
 });
