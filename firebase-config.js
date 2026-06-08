dd772dd273ea9e161220e9ba0b71195b5c1adb30
falaestudante654-debug <falaestudante654@gmail.com>
2026-06-08 06:35:47 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,14 +1,14 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "...",
-  authDomain: "...",
-  projectId: "...",
-  storageBucket: "...",
-  messagingSenderId: "...",
-  appId: "...",
-  measurementId: "..."
+  apiKey: "AIzaSyCKpxwqS8RTAh3C5Z4caUw2cxpUu8EK3HE",
+  authDomain: "fala-estudante.firebaseapp.com",
+  projectId: "fala-estudante",
+  storageBucket: "fala-estudante.firebasestorage.app",
+  messagingSenderId: "1066764400190",
+  appId: "1:1066764400190:web:914dc1696a497f939bc36f",
+  measurementId: "G-LFFX57YH6Z"
 };
 
 const app = initializeApp(firebaseConfig);
